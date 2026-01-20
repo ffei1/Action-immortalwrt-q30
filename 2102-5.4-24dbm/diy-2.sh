ca90385106c2fa158e06d850104e5c2fb721f5fe
ffei1 <121517219@qq.com>
2026-01-20 21:55:28 +0800

    update

--- 2102-5.4-24dbm/diy-2.sh
+++ 2102-5.4-24dbm/diy-2.sh
@@ -14,4 +14,10 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/21_ethinfo.js package/emortal/autocore/files/generic/21_ethinfo.js
 cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/02_network target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+mkdir -p target/linux/mediatek/mt7981/base-files/usr/sbin
+mkdir -p target/linux/mediatek/mt7981/base-files/etc/uci-defaults
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/dhcp-watchdog target/linux/mediatek/mt7981/base-files/usr/sbin/dhcp-watchdog
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/02-setup-cron target/linux/mediatek/mt7981/base-files/etc/uci-defaults/02-setup-cron
+chmod 755 target/linux/mediatek/mt7981/base-files/usr/sbin/dhcp-watchdog
+chmod 755 target/linux/mediatek/mt7981/base-files/etc/uci-defaults/02-setup-cron
 
