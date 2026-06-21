6d3bbed95335b1e4122ce7704409353d765f28b6
ffei1 <166067404+ffei1@users.noreply.github.com>
2026-06-21 15:15:49 +0800

    Update script to copy additional patch files

--- 2102-5.4-24dbm-7.6.7.2/diy-2.sh
+++ 2102-5.4-24dbm-7.6.7.2/diy-2.sh
@@ -12,6 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/21_ethinfo.js package/emortal/autocore/files/generic/21_ethinfo.js
-cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/02_network target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/022-fix-rrm-snprintf-error.patch package/mtk/drivers/mt_wifi/patches-7672/022-fix-rrm-snprintf-error.patch
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/023-fix-qos-peer_vendor_spec_action-frame-check.patch package/mtk/drivers/mt_wifi/patches-7672/023-fix-qos-peer_vendor_spec_action-frame-check.patch
 
