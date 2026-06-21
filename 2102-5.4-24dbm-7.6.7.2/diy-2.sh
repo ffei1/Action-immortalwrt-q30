ee6f01ccdee23dbfa6e9d5b4b925d2f29b171435
ffei1 <166067404+ffei1@users.noreply.github.com>
2026-06-21 15:30:47 +0800

    Update diy-2.sh

--- 2102-5.4-24dbm-7.6.7.2/diy-2.sh
+++ 2102-5.4-24dbm-7.6.7.2/diy-2.sh
@@ -12,4 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm-7.6.7.2/023-fix-qos-peer_vendor_spec_action-frame-check.patch package/mtk/drivers/mt_wifi/patches-7672/023-fix-qos-peer_vendor_spec_action-frame-check.patch
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm-7.6.7.2/022-fix-rrm-snprintf-error.patch package/mtk/drivers/mt_wifi/patches-7672/022-fix-rrm-snprintf-error.patch
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm-7.6.7.2/21_ethinfo.js package/emortal/autocore/files/generic/21_ethinfo.js
+cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm-7.6.7.2/02_network target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
